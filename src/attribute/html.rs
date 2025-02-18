834fd1bf34e84d10ce80cc116ff8d84670f6feb4
Brad Searle <bradleysearle@gmail.com>
2025-02-18 10:33:13 +0000

    arename

---
+++ src/attribute/html.rs
@@ -0,0 +1,411 @@
+use std::fmt::Display;
+
+use log::warn;
+
+use crate::tag::Tag;
+
+use super::{
+    Alt, Az, Charset, Content, Height, Href, HttpEquiv, Id, Lang, Name, Rel, Src, Target, Type,
+    Width,
+};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum LinkTarget {
+    Blank,
+    Parent,
+    Slf,
+    Top,
+    UnfencedTop,
+}
+impl Display for LinkTarget {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LinkTarget::Blank => write!(f, "_blank"),
+            LinkTarget::Parent => write!(f, "_parent"),
+            LinkTarget::Slf => write!(f, "_self"),
+            LinkTarget::Top => write!(f, "_top"),
+            LinkTarget::UnfencedTop => write!(f, "_unfencedTop"),
+        }
+    }
+}
+
+impl From<LinkTarget> for String {
+    fn from(target: LinkTarget) -> Self {
+        target.to_string()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Attr {
+    Alt,
+    Az,
+    Charset,
+    Content,
+    Class,
+    Data,
+    Defer,
+    Height,
+    Href,
+    HttpEquiv,
+    Id,
+    Lang,
+    Name,
+    Src,
+    Target,
+    Type,
+    Rel,
+    Width,
+}
+impl Display for Attr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Attr::Alt => write!(f, "alt"),
+            Attr::Az => write!(f, "as"),
+            Attr::Charset => write!(f, "charset"),
+            Attr::Content => write!(f, "content"),
+            Attr::Class => write!(f, "class"),
+            Attr::Data => write!(f, "data"),
+            Attr::Defer => write!(f, "defer"),
+            Attr::Height => write!(f, "height"),
+            Attr::Href => write!(f, "href"),
+            Attr::HttpEquiv => write!(f, "http-equiv"),
+            Attr::Id => write!(f, "id"),
+            Attr::Lang => write!(f, "lang"),
+            Attr::Name => write!(f, "name"),
+            Attr::Src => write!(f, "src"),
+            Attr::Target => write!(f, "target"),
+            Attr::Type => write!(f, "type"),
+            Attr::Rel => write!(f, "rel"),
+            Attr::Width => write!(f, "width"),
+        }
+    }
+}
+impl Attr {
+    pub fn all() -> &'static [Attr] {
+        &[
+            Attr::Alt,
+            Attr::Az,
+            Attr::Id,
+            Attr::Class,
+            Attr::Charset,
+            Attr::Content,
+            Attr::Data,
+            Attr::Defer,
+            Attr::Height,
+            Attr::Href,
+            Attr::HttpEquiv,
+            Attr::Lang,
+            Attr::Name,
+            Attr::Rel,
+            Attr::Src,
+            Attr::Target,
+            Attr::Type,
+            Attr::Width,
+        ]
+    }
+    pub fn global() -> &'static [Attr] {
+        &[
+            //
+            Attr::Id,
+            Attr::Class,
+            Attr::Data,
+            Attr::Lang,
+        ]
+    }
+}
+
+// HTML data-* attributes
+#[derive(Debug, Clone)]
+pub struct Data(pub String, pub String);
+
+impl Display for Data {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, r#"{}-{}="{}""#, Attr::Data, self.0, self.1)
+    }
+}
+
+// Boolean attributes
+#[derive(Debug, Clone)]
+pub struct Defer;
+
+impl Display for Defer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Attr::Defer)
+    }
+}
+
+// Vector Attributes
+#[derive(Debug, Clone)]
+pub struct Class(Vec<String>);
+
+impl Display for Class {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Class(values) => {
+                write!(f, r#"{}="{}""#, Attr::Class, values.join(" "))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Attrs {
+    // Global
+    pub alt: Option<Alt>,
+    pub az: Option<Az>,
+    pub id: Option<Id>,
+    pub class: Option<Class>,
+    pub charset: Option<Charset>,
+    pub content: Option<Content>,
+    pub data: Option<Data>,
+    pub defer: Option<Defer>,
+    pub height: Option<Height>,
+    pub href: Option<Href>,
+    pub http_equiv: Option<HttpEquiv>,
+    pub lang: Option<Lang>,
+    pub name: Option<Name>,
+    pub rel: Option<Rel>,
+    pub src: Option<Src>,
+    pub target: Option<Target>,
+    pub typ: Option<Type>,
+    pub width: Option<Width>,
+}
+
+impl Attrs {
+    pub fn new() -> AttrsBuilder {
+        AttrsBuilder::new()
+    }
+    pub fn get_attrs(&self, tag: &Tag) -> Vec<String> {
+        let tag_attributes = Tag::attributes(tag);
+        let mut attributes = vec![];
+
+        if self.alt.is_some() && validate_attrs(tag, &Attr::Alt, &tag_attributes) {
+            attributes.push(self.alt.as_ref().unwrap().to_string())
+        }
+        if self.az.is_some() && validate_attrs(tag, &Attr::Az, &tag_attributes) {
+            attributes.push(self.az.as_ref().unwrap().to_string())
+        }
+        if self.charset.is_some() && validate_attrs(tag, &Attr::Charset, &tag_attributes) {
+            attributes.push(self.charset.as_ref().unwrap().to_string())
+        }
+        if self.class.is_some() && validate_attrs(tag, &Attr::Class, &tag_attributes) {
+            attributes.push(self.class.as_ref().unwrap().to_string())
+        }
+        if self.content.is_some() && validate_attrs(tag, &Attr::Content, &tag_attributes) {
+            attributes.push(self.content.as_ref().unwrap().to_string())
+        }
+        if self.data.is_some() && validate_attrs(tag, &Attr::Data, &tag_attributes) {
+            attributes.push(self.data.as_ref().unwrap().to_string())
+        }
+        if self.defer.is_some() && validate_attrs(tag, &Attr::Defer, &tag_attributes) {
+            attributes.push(self.defer.as_ref().unwrap().to_string())
+        }
+        if self.height.is_some() && validate_attrs(tag, &Attr::Height, &tag_attributes) {
+            attributes.push(self.height.as_ref().unwrap().to_string())
+        }
+        if self.href.is_some() && validate_attrs(tag, &Attr::Href, &tag_attributes) {
+            attributes.push(self.href.as_ref().unwrap().to_string())
+        }
+        if self.http_equiv.is_some() && validate_attrs(tag, &Attr::HttpEquiv, &tag_attributes) {
+            attributes.push(self.http_equiv.as_ref().unwrap().to_string())
+        }
+        if self.id.is_some() && validate_attrs(tag, &Attr::Id, &tag_attributes) {
+            attributes.push(self.id.as_ref().unwrap().to_string())
+        }
+        if self.lang.is_some() && validate_attrs(tag, &Attr::Lang, &tag_attributes) {
+            attributes.push(self.lang.as_ref().unwrap().to_string())
+        }
+        if self.name.is_some() && validate_attrs(tag, &Attr::Name, &tag_attributes) {
+            attributes.push(self.name.as_ref().unwrap().to_string())
+        }
+        if self.src.is_some() && validate_attrs(tag, &Attr::Src, &tag_attributes) {
+            attributes.push(self.src.as_ref().unwrap().to_string())
+        }
+        if self.target.is_some() && validate_attrs(tag, &Attr::Target, &tag_attributes) {
+            attributes.push(self.target.as_ref().unwrap().to_string())
+        }
+        if self.typ.is_some() && validate_attrs(tag, &Attr::Type, &tag_attributes) {
+            attributes.push(self.typ.as_ref().unwrap().to_string())
+        }
+        if self.rel.is_some() && validate_attrs(tag, &Attr::Rel, &tag_attributes) {
+            attributes.push(self.rel.as_ref().unwrap().to_string())
+        }
+        if self.width.is_some() && validate_attrs(tag, &Attr::Width, &tag_attributes) {
+            attributes.push(self.width.as_ref().unwrap().to_string())
+        }
+        attributes
+    }
+}
+
+pub struct AttrsBuilder {
+    pub alt: Option<Alt>,
+    pub az: Option<Az>,
+    pub charset: Option<Charset>,
+    pub class: Option<Class>,
+    pub content: Option<Content>,
+    pub data: Option<Data>,
+    pub defer: Option<Defer>,
+    pub height: Option<Height>,
+    pub href: Option<Href>,
+    pub http_equiv: Option<HttpEquiv>,
+    pub id: Option<Id>,
+    pub lang: Option<Lang>,
+    pub name: Option<Name>,
+    pub rel: Option<Rel>,
+    pub src: Option<Src>,
+    pub target: Option<Target>,
+    pub typ: Option<Type>,
+    pub width: Option<Width>,
+}
+impl Default for AttrsBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl AttrsBuilder {
+    pub fn new() -> Self {
+        AttrsBuilder {
+            alt: None,
+            az: None,
+            charset: None,
+            class: None,
+            content: None,
+            data: None,
+            defer: None,
+            height: None,
+            href: None,
+            http_equiv: None,
+            id: None,
+            lang: None,
+            name: None,
+            rel: None,
+            src: None,
+            target: None,
+            typ: None,
+            width: None,
+        }
+    }
+    pub fn alt(mut self, alt: impl Into<String>) -> Self {
+        self.alt = Some(Alt(alt.into()));
+        self
+    }
+
+    pub fn az(mut self, az: impl Into<String>) -> Self {
+        self.az = Some(Az(az.into()));
+        self
+    }
+
+    pub fn charset(mut self, charset: impl Into<String>) -> Self {
+        self.charset = Some(Charset(charset.into()));
+        self
+    }
+
+    pub fn content(mut self, content: impl Into<String>) -> Self {
+        self.content = Some(Content(content.into()));
+        self
+    }
+
+    pub fn class(mut self, class: Vec<impl Into<String>>) -> Self {
+        let string_vec: Vec<String> = class.into_iter().map(|s| s.into()).collect();
+        self.class = Some(Class(string_vec));
+        self
+    }
+
+    pub fn data(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.data = Some(Data(key.into(), value.into()));
+        self
+    }
+
+    pub fn defer(mut self) -> Self {
+        self.defer = Some(Defer);
+        self
+    }
+
+    pub fn height(mut self, height: impl Into<String>) -> Self {
+        self.height = Some(Height(height.into()));
+        self
+    }
+
+    pub fn href(mut self, href: impl Into<String>) -> Self {
+        self.href = Some(Href(href.into()));
+        self
+    }
+
+    pub fn http_equiv(mut self, http_equiv: impl Into<String>) -> Self {
+        self.http_equiv = Some(HttpEquiv(http_equiv.into()));
+        self
+    }
+
+    pub fn id(mut self, id: impl Into<String>) -> Self {
+        self.id = Some(Id(id.into()));
+        self
+    }
+
+    pub fn lang(mut self, lang: impl Into<String>) -> Self {
+        self.lang = Some(Lang(lang.into()));
+        self
+    }
+
+    pub fn name(mut self, name: impl Into<String>) -> Self {
+        self.name = Some(Name(name.into()));
+        self
+    }
+
+    pub fn rel(mut self, rel: impl Into<String>) -> Self {
+        self.rel = Some(Rel(rel.into()));
+        self
+    }
+
+    pub fn src(mut self, src: impl Into<String>) -> Self {
+        self.src = Some(Src(src.into()));
+        self
+    }
+
+    pub fn target(mut self, target: LinkTarget) -> Self {
+        self.target = Some(Target(target.into()));
+        self
+    }
+
+    pub fn typ(mut self, typ: impl Into<String>) -> Self {
+        self.typ = Some(Type(typ.into()));
+        self
+    }
+
+    pub fn width(mut self, width: impl Into<String>) -> Self {
+        self.width = Some(Width(width.into()));
+        self
+    }
+
+    pub fn build(self) -> Attrs {
+        Attrs {
+            alt: self.alt,
+            az: self.az,
+            charset: self.charset,
+            class: self.class,
+            content: self.content,
+            data: self.data,
+            defer: self.defer,
+            height: self.height,
+            href: self.href,
+            http_equiv: self.http_equiv,
+            id: self.id,
+            lang: self.lang,
+            name: self.name,
+            rel: self.rel,
+            src: self.src,
+            target: self.target,
+            typ: self.typ,
+            width: self.width,
+        }
+    }
+}
+
+// Check if the attributes supplied are valid for the HTML tag variant.
+fn validate_attrs(tag: &Tag, check_attribute: &Attr, valid_attributes: &[Attr]) -> bool {
+    if valid_attributes.contains(check_attribute) {
+        true
+    } else {
+        warn!("HTML tag '{tag}' does not support the '{check_attribute}' attribute");
+        false
+    }
+}
---
+++ src/element/html.rs
@@ -0,0 +1,130 @@
+use std::fmt::Display;
+
+use crate::attribute::Attrs;
+use crate::tag::Tag;
+
+#[derive(Debug, Clone)]
+pub struct ElementBuilder {
+    pub tag: Tag,
+    pub attrs: Option<Attrs>,
+    pub text: Option<String>,
+    pub content: Option<String>,
+    pub children: Option<Vec<Element>>,
+}
+
+impl ElementBuilder {
+    pub fn new(tag: Tag) -> Self {
+        ElementBuilder {
+            tag,
+            attrs: None,
+            text: None,
+            content: None,
+            children: None,
+        }
+    }
+
+    // region:    ===== Global Attributes ===== //
+    pub fn attrs(mut self, attrs: Attrs) -> Self {
+        self.attrs = Some(attrs);
+        self
+    }
+    // target
+
+    // endregion: ===== a tag attributes ===== //
+
+    // Text within a tag
+    pub fn text(mut self, text: impl Into<String>) -> Self {
+        self.text = Some(text.into());
+        self
+    }
+
+    // Content within a opening and closing tag
+    pub fn content(mut self, content: impl Into<String>) -> Self {
+        self.content = Some(content.into());
+        self
+    }
+
+    // Nested tag
+    pub fn children(mut self, children: Vec<Element>) -> Self {
+        self.children = Some(children);
+        self
+    }
+
+    pub fn build(self) -> Element {
+        Element {
+            tag: self.tag,
+            attrs: self.attrs,
+            text: self.text,
+            content: self.content,
+            children: self.children,
+        }
+    }
+}
+
+// blah
+#[derive(Debug, Clone)]
+pub struct Element {
+    pub tag: Tag,
+    pub attrs: Option<Attrs>,
+    pub text: Option<String>,
+    pub content: Option<String>,
+    pub children: Option<Vec<Element>>,
+}
+impl Display for Element {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let fmt_str = self.make_tag();
+        write!(f, "{fmt_str}")
+    }
+}
+
+impl Element {
+    // Create a HTML opening tag.
+    pub fn open_tag(tag: &Tag, value: impl Into<String>) -> String {
+        let value = value.into();
+        match tag {
+            Tag::Comment => format!("<!-- {} -->", value),
+            _ => {
+                // let value = value.into();
+                if !value.is_empty() {
+                    format!("<{} {}>", tag, value)
+                } else {
+                    format!("<{}>", tag)
+                }
+            }
+        }
+    }
+    // Create a HTML closing tag.
+    pub fn close_tag(tag: &Tag) -> String {
+        match tag {
+            // Void tags are self closing.
+            Tag::Doctype | Tag::Meta | Tag::Comment | Tag::Br | Tag::Img => "".to_string(),
+            // All other tags have a corresponding closing tag
+            _ => format!("</{}>", tag),
+        }
+    }
+    pub fn make_tag(&self) -> String {
+        let content = self
+            .content
+            .as_ref()
+            .map_or(String::new(), |content| content.to_string());
+
+        let children: String = self.children.as_ref().map_or(String::new(), |children| {
+            children.iter().map(|child| child.to_string()).collect()
+        });
+
+        let attributes =
+            if let Some(attrs) = self.attrs.as_ref() { attrs.get_attrs(&self.tag) } else { vec![] };
+
+        let open_tag = if !attributes.is_empty() {
+            Element::open_tag(&self.tag, attributes.join(" "))
+        } else if self.text.is_some() {
+            Element::open_tag(&self.tag, self.text.as_ref().unwrap())
+        } else {
+            Element::open_tag(&self.tag, "")
+        };
+
+        let close_tag = Element::close_tag(&self.tag);
+
+        format!(r#"{open_tag}{content}{children}{close_tag}"#)
+    }
+}
